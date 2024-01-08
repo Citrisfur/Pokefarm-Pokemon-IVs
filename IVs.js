b351c0a92553fd1171763cccfd1b05e07865b766
Nathan Coy <nathanscoy@gmail.com>
2024-01-08 01:44:40 -0700

    Switch to JSONesque inventory format instead of strings, also includes all information about Pokemon instead of deciding relevance here

--- IVs.js
+++ IVs.js
@@ -17,28 +17,27 @@ $(button).on("click", () => {
 				pkmnListPromiseList.push(new Promise((resolve, reject) => {
 					unsafeWindow.ajax("fields/pkmnlist", {
 						"fieldid": i,
-            "tooltip": "train"
+						"tooltip": "train"
 					}).success((response) => {
 						console.log("Retrieved Pokemon Field " + i + "/" + numberOfFields);
 						$(response.html).find(".fieldmontip").each(function() {
-							var pokemonName = $(this).find(".icons").parent().text().substring(10);
-							var pokemonForm = $(this).find(".forme").length != 0 ? "(" + $(this).find(".forme").text().substring(7) + ") " : "";
+							var pokemonName = $(this).find(".icons").parent().text().substring(10, $(this).find(".icons").parent().text().length - 1);
+							var pokemonForm = $(this).find(".forme").length != 0 ? "(" + $(this).find(".forme").text().substring(7) + ")" : "";
 							var pokemonGender = "";
 							$(this).find("img[title]").each(function() {
 								pokemonGender += $(this).attr("title");
 							});
 							var pokemonIVs = "";
 							var pokemonPerfectIVs = 0;
-              var pokemonIVTotal = 0;
+							var pokemonIVTotal = 0;
 							$(this).find(".tooltip_content").parent().find("span").slice(0,6).each(function() {
 								pokemonIVs += $(this).text() + "/";
 								if ($(this).text() == "31") { ++pokemonPerfectIVs; }
 								pokemonIVTotal += parseInt($(this).text());
 							})
-							pokemonIVs = pokemonIVs.substring(0, pokemonIVs.length-1);
-							pokemonIVTotal = " = " + pokemonIVTotal;
-							var pokemonNature = pokemonPerfectIVs == 6 ? $(this).find(".item").prev().clone().children().remove().end().text(): "";
-							inventory.push(pokemonName + pokemonForm + pokemonGender + ": (" + pokemonPerfectIVs + ") " + pokemonIVs + pokemonIVTotal + pokemonNature);
+							pokemonIVs = pokemonIVs.substring(0, pokemonIVs.length - 1);
+							var pokemonNature = $(this).find(".item").prev().clone().children().remove().end().text().substring(1);
+							inventory.push([pokemonName,pokemonForm,pokemonGender,pokemonPerfectIVs,pokemonIVs,pokemonIVTotal,pokemonNature]);
 							++totalPokemon;
 						});
 						resolve(response);
