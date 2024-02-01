6bced5d4c54213f6ad4cb3bfd8ec3cd549ca63b3
Nathan Coy <nathanscoy@gmail.com>
2024-01-31 18:15:09 -0700

    script will save to and load from multiple inventory files, one created per 2000 pokemon (50 full fields)

--- IVs.js
+++ IVs.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// todo: remove para from forms i.e. (Othrus)
-// todo: add pokemon name to json
-// todo: make gender its own key value pair instead of in attrs.
 // todo: auto add in pokemon that aren't in inventory
 // todo: compare ivs of pokemon
 // todo: display ivs on field tooltips or side context menu
@@ -10,7 +7,7 @@
 
 var $ = unsafeWindow.$;
 var inventory = [];
-var fileID = 0;
+var fileIDs = [];
 
 let button = $("<li data-name=\"IV Inventory\"><a title=\"Count Inventory\" style=\"cursor: pointer;\"><img src=\"https://pfq-static.com/img/navbar/dex.png\"> (...) Inventory </a></li>");
 $("#announcements > ul > li.spacer").eq(0).before(button);
@@ -22,128 +19,143 @@ function loadInventory() {
 		}).success((response) => {
 			console.log("Retrieved list of user's notepad files");
 
+			let inventoryFilePromiseList = [];
 			$(response.html).find("a[data-file]").each(function(index) {
-				if ($(this).text() == "PokemonInventory.json") {
-					fileID = $(this).data("file");
-					console.log("Inventory found!");
+				if ($(this).text().slice(0, 16) == "PokemonInventory" && $(this).text().slice(-5) == ".json") {
+					let fileID = $(this).data("file");
+					fileIDs.push(fileID);
 
-					new Promise((resolve, reject) => {
+					inventoryFilePromiseList.push(new Promise((resolve, reject) => {
 						unsafeWindow.ajax("farm/notepad", {
 							"directory": null,
 							"file": fileID
 						}).success((response) => {
-							inventory = JSON.parse($(response.html).find("textarea").text());
-							console.log(inventory);
-							$(button).find("a").contents().filter(function(){
-								return (this.nodeType == 3);
-							}).replaceWith(" (" + inventory.length + ") Inventory");
+							$.merge(inventory, JSON.parse($(response.html).find("textarea").text()));
 							resolve(response);
 						}).failure((response) => {
-							console.log("Error reading Pokemon Field " + i);
+							console.log("Error loading notepad inventory file " + fileIDs.length);
 							reject(response);
 						});
-					});
-					// may rewrite each function to add break here
+					}));
 				}
 			});
 
-			if (!fileID) {
+			if (inventoryFilePromiseList.length) {
+				console.log(inventoryFilePromiseList.length + " inventory files found...");
+				inventoryFilePromiseList.slice(-1)[0].then(function() {
+					$(button).find("a").contents().filter(function(){
+            return (this.nodeType == 3);
+          }).replaceWith(" (" + inventory.length + ") Inventory");
+          console.log(inventory);
+				});
+			} else {
 				console.log("Inventory does not exist, creating...");
 				new Promise((resolve, reject) => {
 					unsafeWindow.ajax("fields/fieldlist", {
 						uid: 0
 					}).success((response) => {
-						var numberOfFields = 5; // testing code
-	//					var numberOfFields = response.fields.length;
-						var pkmnListPromiseList = [];
+						let totalFields = 10; // testing code
+//						let totalFields = response.fields.length;
 
-						for (let i = 0; i < numberOfFields; i++) {
+						let pkmnListPromiseList = [];
+						for (let fieldID = 0; fieldID < totalFields; fieldID++) {
 							pkmnListPromiseList.push(new Promise((resolve, reject) => {
 								unsafeWindow.ajax("fields/pkmnlist", {
-									"fieldid": i,
+									"fieldid": fieldID,
 									"tooltip": "train"
 								}).success((response) => {
-									console.log("Retrieved Pokemon Field " + i + "/" + numberOfFields);
+									console.log("Retrieved Pokemon Field " + (fieldID + 1) + "/" + totalFields);
 									$(response.html).find(".fieldmontip").each(function() {
-										var pokemon = {};
+										let pokemon = {};
+										pokemon["id"] = $(this).find("h3").eq(0).find("a").attr("href").slice(-5);
+										pokemon["name"] = $(this).find("h3").eq(0).text();
 										pokemon["species"] = $(this).find(".icons").parent().text().substring(10, $(this).find(".icons").parent().text().length - 1);
-										pokemon["form"] = $(this).find(".forme").length != 0 ? "(" + $(this).find(".forme").text().substring(7) + ")" : null;
 
-										var pokemonAttributes = [];
-										$(this).find("img[title]").each(function() {
-											pokemonAttributes.push($(this).attr("title"));
+										let pokemonAttributes = [];
+										$(this).find(".icons").children().each(function() {
+											pokemonAttributes.push($(this).attr("title").substring(1, $(this).attr("title").length - 1));
 										});
 
+										pokemon["gender"] = pokemonAttributes.shift();
+										pokemon["form"] = $(this).find(".forme").length != 0 ? $(this).find(".forme").text().substring(7) : null;
 										pokemon["attributes"] = pokemonAttributes;
+										pokemon["nature"] = $(this).find(".item").prev().clone().children().remove().end().text().substring(1);
 
-										var html_pokemonIVs = $(this).find(".tooltip_content").parent().find("span").slice(0,6); // if not sliced 7-12 indexes are evs
-										var pokemonIVs = {};
+										let html_pokemonIVs = $(this).find("span[data-tooltip]").slice(0,6); // if not sliced 7-12 indexes are evs
+
+										let pokemonIVs = {};
 										pokemonIVs["health"] = parseInt($(html_pokemonIVs[0]).text());
 										pokemonIVs["attack"] = parseInt($(html_pokemonIVs[1]).text());
-										pokemonIVs["defence"] = parseInt($(html_pokemonIVs[2]).text());
+										pokemonIVs["defense"] = parseInt($(html_pokemonIVs[2]).text());
 										pokemonIVs["special_attack"] = parseInt($(html_pokemonIVs[3]).text());
-										pokemonIVs["special_defence"] = parseInt($(html_pokemonIVs[4]).text());
+										pokemonIVs["special_defense"] = parseInt($(html_pokemonIVs[4]).text());
 										pokemonIVs["speed"] = parseInt($(html_pokemonIVs[5]).text());
 
-										var pokemonPerfectIVs = 0;
-										var pokemonIVTotal = 0;
-										for (var x = 0; x < html_pokemonIVs.length; ++x){
+										let pokemonPerfectIVs = 0;
+										let pokemonIVTotal = 0;
+										for (let x = 0; x < html_pokemonIVs.length; ++x){
 											if ($(html_pokemonIVs[x]).text() == "31") { ++pokemonPerfectIVs; }
 											pokemonIVTotal += parseInt($(html_pokemonIVs[x]).text());
 										}
 
-										pokemon["perfect_ivs"] = pokemonPerfectIVs;
 										pokemon["ivs"] = pokemonIVs;
 										pokemon["iv_total"] = pokemonIVTotal;
-										pokemon["nature"] = $(this).find(".item").prev().clone().children().remove().end().text().substring(1);
+										pokemon["perfect_ivs"] = pokemonPerfectIVs;
+
 										inventory.push(pokemon);
 									});
 									resolve(response);
 								}).failure((response) => {
-									console.log("Error reading Pokemon Field " + i);
+									console.log("Error reading Pokemon Field " + fieldID);
 									reject(response);
 								});
 							}));
 						}
 
-						pkmnListPromiseList[numberOfFields-1].then(function() {
+						pkmnListPromiseList.slice(-1)[0].then(function() {
 							console.log("Read all Pokemon fields and built inventory");
-							console.log(inventory);
-							new Promise((resolve, reject) => {
-								unsafeWindow.ajax("farm/notepad", {
-									"directory": "",
-									"mode": "newfile",
-									"save": {
-										"name": "PokemonInventory.json"
-									}
-								}).success((response) => {
-									fileID = $($(response.html)[1]).data("fileform");
-									console.log("Created file PokemonInventory.json in user's notepad");
-									new Promise((resolve, reject) => {
-										unsafeWindow.ajax("farm/notepad", {
-											"file": fileID,
-											"mode": "save",
-											"save": {
-												"name": "PokemonInventory.json",
-												"content": JSON.stringify(inventory)
-											}
-										}).success((response) => {
-											console.log("Wrote inventory to PokemonInventory.json");
-											resolve(response);
-										}).failure((response) => {
-											console.log("Error reading Pokemon field list");
-											reject(response);
+
+							for (let fileNumber = 1; fileNumber <= Math.ceil(inventory.length / 2000); fileNumber++) {
+								new Promise((resolve, reject) => {
+									unsafeWindow.ajax("farm/notepad", {
+										"directory": "",
+										"mode": "newfile",
+										"save": {
+											"name": "PokemonInventory" + fileNumber + ".json"
+										}
+									}).success((response) => {
+										let fileID = $($(response.html)[1]).data("fileform");
+										fileIDs.push(fileID);
+										console.log("Created file PokemonInventory" + fileNumber + ".json in user's notepad");
+										new Promise((resolve, reject) => {
+											unsafeWindow.ajax("farm/notepad", {
+												"file": fileID,
+												"mode": "save",
+												"save": {
+													"name": "PokemonInventory" + fileNumber + ".json",
+													"content": JSON.stringify(inventory.slice(fileNumber * 2000 - 2000, fileNumber * 2000))
+												}
+											}).success((response) => {
+												console.log("Wrote inventory to PokemonInventory" + fileNumber + ".json");
+												resolve(response);
+											}).failure((response) => {
+												console.log("Error reading Pokemon field list");
+												reject(response);
+											});
 										});
+										resolve(response);
+									}).failure((response) => {
+										console.log("Error creating file PokemonInventory" + fileNumber + ".json");
+										reject(response);
 									});
-									resolve(response);
-								}).failure((response) => {
-									console.log("Error creating PokemonInventory.json file");
-									reject(response);
 								});
-							});
+							}
+
 							$(button).find("a").contents().filter(function(){
 								return (this.nodeType == 3);
 							}).replaceWith(" (" + inventory.length + ") Inventory");
+
+							console.log(inventory);
 						});
 						resolve(response);
 					}).failure((response) => {
@@ -162,25 +174,27 @@ function loadInventory() {
 loadInventory();
 
 $(button).on("click", () => {
-	let popup = confirm("WARNING!\n\nThis will remove and rebuild your current inventory file (PokemonInventory.json) in your Pokefarm notepad. This should only be run if absolutely necessary.\n\nNote: If your inventory file is missing, the script will rebuild it automatically without the need to run this operation. To do so, simply refresh this page.\n\nThe rebuild will take longer the more Pokemon you own (about a minute per 3k Pokemon). Click OK to continue.");
+	let popup = confirm("WARNING!\n\nThis will remove and rebuild your inventory files (PokemonInventoryN.json) in your Pokefarm notepad. This should only be run if absolutely necessary.\n\nNote: If your inventory files are missing, the script will rebuild them automatically without the need to run this operation. To do so, simply refresh this page.\n\nThe rebuild will take longer the more Pokemon you own and the busier the site is. Click OK to continue.");
 	if (popup) {
-		new Promise((resolve, reject) => {
-			unsafeWindow.ajax("farm/notepad", {
-				"mode": "command",
-				"command": "delete",
-				"operands": {
-					"type": "file",
-					"id": fileID
-				}
-			}).success((response) => {
-				console.log("Deleted current PokemonInventory.json");
-				resolve(response);
-			}).failure((response) => {
-				console.log("Error deleting file PokemonInventory.json");
-				reject(response);
+		for (let fileID of fileIDs) {
+			new Promise((resolve, reject) => {
+				unsafeWindow.ajax("farm/notepad", {
+					"mode": "command",
+					"command": "delete",
+					"operands": {
+						"type": "file",
+						"id": fileID
+					}
+				}).success((response) => {
+					console.log("Deleted a PokemonInventory.json file");
+					resolve(response);
+				}).failure((response) => {
+					console.log("Error deleting a PokemonInventory.json file");
+					reject(response);
+				});
 			});
-		});
-		fileID = 0;
+		}
+		fileIDs = [];
 		inventory = [];
 		$(button).find("a").contents().filter(function(){
 			return (this.nodeType == 3);
